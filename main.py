4178adba75259d05e92147f8234d34a2b23c85e0
callmepvp <mrxgameing@gmail.com>
2024-07-02 23:46:41 +0300

    added many things

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-﻿class Muusika:
-	def __init__(self, nimi):
+﻿import itertools as it
+
+class Muusika:
+	def __init__(self, nimi, emotsioonid):
 		self.nimi = nimi
-		#self.emotsioonid = 
+		self.emotsioonid = emotsioonid
+
+	def getEmotsioonid(self):
+		return self.emotsioonid
+	
+	def kasOmabEmotsiooniPaari(self, paar):
+		return self.getEmotsioonid()[paar[0]] == 1 and self.getEmotsioonid()[paar[1]] == 1
 		
 
 def main():
@@ -14,14 +22,32 @@ def main():
 			grupp = []
 		else:
 			grupp.append(i.strip())
-	for i in tekstigrupid:
-		print(i)
-	
 
 	kõikMeeleolud = set()
+	kõikLaulud = []
 	for grupp in tekstigrupid:
+		kõikLaulud.append(grupp[0])
 		for sõna in grupp[1:]:
 			kõikMeeleolud.add(sõna)
-			
+	
 	print(kõikMeeleolud)
+
+	lood = []
+	for laul in tekstigrupid:
+		emotsioonid = {j : 1 if j in laul else 0 for j in kõikMeeleolud}
+		lugu = Muusika(laul[0], emotsioonid)
+		lood.append(lugu)
+
+	meeleoluPaarid = list(it.combinations(kõikMeeleolud, 2))
+	paaridDict = {i : 0 for i in meeleoluPaarid}
+	for i in meeleoluPaarid:
+		for j in lood:
+			if j.kasOmabEmotsiooniPaari(i):
+				paaridDict[i] += 1
+
+	suurimVäärtus = max(paaridDict.values())
+	for j in paaridDict:
+		paaridDict[j] = paaridDict[j] / suurimVäärtus
+	
+	print(paaridDict)
 main()
