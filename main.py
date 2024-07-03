92e93390bac5ee76e4c99c4c524fac517be3e85c
Kuulimagnaat <kaarelparve@gmail.com>
2024-07-03 17:00:25 +0300

    Töötab. Lood saavad oale automaatselt pädevaid meeleolusid.

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ class Muusika:
 	
 	def kasOmabEmotsiooniPaari(self, paar):
 		return self.getEmotsioonid()[paar[0]] == 1 and self.getEmotsioonid()[paar[1]] == 1
+	
+	def LiidaEmotsioonile(self, emotsioon, väärtus):
+		self.emotsioonid[emotsioon] += väärtus
+
+	def ÜmardaEmotsioonid(self, kohti):
+		for i in self.emotsioonid:
+			self.emotsioonid[i] = round(self.emotsioonid[i], kohti)
 		
 
 def main():
@@ -30,7 +37,6 @@ def main():
 		for sõna in grupp[1:]:
 			kõikMeeleolud.add(sõna)
 	
-	print(kõikMeeleolud)
 
 	lood = []
 	for laul in tekstigrupid:
@@ -47,7 +53,36 @@ def main():
 
 	suurimVäärtus = max(paaridDict.values())
 	for j in paaridDict:
-		paaridDict[j] = paaridDict[j] / suurimVäärtus
+		paaridDict[j] = paaridDict[j] / len(lood)
 	
-	print(paaridDict)
+	
+	for i in lood:
+		ühegaEmotsioonid = []
+		for j in i.getEmotsioonid():
+			if i.getEmotsioonid()[j] == 1:
+				ühegaEmotsioonid.append(j)
+
+		# Selles dictis on paaridDictist võetud keyvalue pairid, mille key-s olevaks emotsioonipaaris on sees element varem loodud niekirja ühegaEmotsioonid mõni element.
+		ühegaPaarid = {}
+		for j in paaridDict:
+			for k in ühegaEmotsioonid:
+				if k in j:
+					ühegaPaarid[j] = paaridDict[j]
+
+		
+		for j in ühegaPaarid:
+			if i.getEmotsioonid()[j[0]] != 1.0:
+				i.LiidaEmotsioonile(j[0], ühegaPaarid[j])
+			elif i.getEmotsioonid()[j[1]] != 1.0:
+				i.LiidaEmotsioonile(j[1], ühegaPaarid[j])
+				
+	
+	for i in lood:
+		print(i.getEmotsioonid())
+
+	for i in lood:
+		i.ÜmardaEmotsioonid(2)
+						
+	
+	# Nüüd tahaks igal lool vaadata tema emotsioone ja juurde liita
 main()
